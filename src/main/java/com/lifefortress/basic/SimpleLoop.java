8b9b2567a4a84541801c34f43f87d68700242235
Sarwo Edi <sarwo.wibowo@bertelsmann.de>
2016-10-05 16:13:19 +0800

    simple loop

---
+++ src/main/java/com/lifefortress/basic/SimpleLoop.java
@@ -0,0 +1,23 @@
+package com.lifefortress.basic;
+
+import java.util.Scanner;
+
+/**
+ * given n = any number <br/>
+ * when 1 < i <= 10  <br />
+ * then print  N * i : result 
+ * 
+ * @author WIBO001
+ *
+ */
+public class SimpleLoop { 
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		int n  = input.nextInt();
+	
+		for (int i = 1; i <= 10; i++) {
+			System.out.println(String.format("%d x %d = %d ", n, i , (n*i)));
+		}
+		
+	}
+}
